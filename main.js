a9ee8cff0a174e36c5321c98e099e1bef200fa4a
Soraya Lallana <sorayalallana29@gmail.com>
2022-03-31 21:37:03 -0300

    desafio entregable 31/3/22

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Variables */
+let referidosClientes = "";
+
+/* Acción */
+alert ("Coloca 5 correos electronicos de tus amigos y accedé a un 50% de descuento en tu compra! Cuando los termines de referir, coloca ESC para que se aplique el descuento")
+for (let i = 0; i <5; i++) {
+    prompt ("escribe el correo de tu amigo")   
+} 
+alert ("Tu descuento se aplicó con éxito")
